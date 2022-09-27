ef140fc80a541f8bbb982053e9da3d17fdb3f054
nameindustry <nameindustry@gmail.com>
2022-09-27 15:32:50 -0400

    [ init ] setup from template

--- src/shell.js
+++ src/shell.js
@@ -3,9 +3,9 @@ import React from "react";
 const ApplicationShell = () => {
     return (
         <React.Fragment>
-            <div className="w-full bg-slate-300">
-                <p className="pl-2 pt-1 pb-1 pr-2 text-sm text-slate-700">
-                    [ SPA.CRA.React - Template ]
+            <div className="w-full pl-2 pt-1 pb-1 pr-2 bg-slate-50">
+                <p className="text-sm text-slate-700 font-bold">
+                    [ N.I.A.M ]
                 </p>
             </div>
         </React.Fragment>
