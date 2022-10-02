2726f631dc18c479673713d455bb1837ca28adfe
nameindustry <nameindustry@gmail.com>
2022-10-02 17:50:12 -0400

    [ feat ] update .env add rapidoc to public folder hook into docs with iframe

--- src/Pages/Docs/index.js
+++ src/Pages/Docs/index.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 export default function Docs() {
+
+    const docsViewerPath = process.env.PUBLIC_URL + "/docs-viewer/index.html";
+
     return (
-        <div><p>Docs</p></div>
+        <div>
+            <p>Docs location@{docsViewerPath}</p>
+
+            <iframe id="inlineFrameExample"
+                title="Inline Frame Example"
+                width="300"
+                height="200"
+                src={docsViewerPath}>
+            </iframe>
+
+        </div>
     )
 }
